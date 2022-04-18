BUG: Adjusts yeojohnson_llf to be -np.inf for low variance

scipy/stats/_morestats.py
@@ -1584,7 +1584,7 @@ def yeojohnson_llf(lmb, data):
 
     # Avoid RuntimeWarning raised by np.log when the variance is too low
     tiny_variance = trans_var < np.finfo(trans_var.dtype).tiny
-    loglike[tiny_variance] = np.inf
+    loglike[tiny_variance] = -np.inf
 
     loglike[~tiny_variance] = (
         -n_samples / 2 * np.log(trans_var[~tiny_variance]))
@@ -1641,11 +1641,7 @@ def yeojohnson_normmax(x, brack=(-2, 2)):
 
     """
     def _neg_llf(lmbda, data):
-        llf = yeojohnson_llf(lmbda, data)
-        # reject likelihoods that are inf which are likely due to small
-        # variance in the transformed space
-        llf[np.isinf(llf)] = -np.inf
-        return -llf
+        return -yeojohnson_llf(lmbda, data)
 
     with np.errstate(invalid='ignore'):
         return optimize.brent(_neg_llf, brack=brack, args=(x,))